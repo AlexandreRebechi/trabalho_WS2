2526fbf4b9d7fd2011ac74738e156dfda3f8a6dd
AlexandeRebechi <alexandrerebechi.pf280@academico.ifsul.edu.br>
2022-07-18 17:18:01 -0300

    sda

--- PrediosTrabalhoWs2/banco.sql
+++ PrediosTrabalhoWs2/banco.sql
@@ -11,8 +11,3 @@ preco:DECIMAL(10,2)
 
 insert into Predios (nome, numApartamentos, endereco, preco) values ('predio1', 10, 'endereco1',true,1500.00), ('predio2', 20, 'endereco2',false,10000.00);
 
-create table usuarios (
-nome_usuario varchar(30) not null primary key, 
-senha varchar(20) not null);
-
-insert into usuarios (nome_usuario, senha) values ('nome' , '1234'), ('nome', '4321');
